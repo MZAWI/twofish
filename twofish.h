a5512252ca55cc4088abde35405c42d78e3aced9
mzawieracz@student.agh.edu.pl <mzawieracz@student.agh.edu.pl>
2026-01-14 23:05:25 +0100

    add initialization function to header file

--- twofish.h
+++ twofish.h
@@ -8,3 +8,5 @@ void twofish_decrypt_block(Twofish_ctx *ctx, uint8_t buf[16]);
 
 void twofish_encrypt_cbc(Twofish_ctx *ctx, uint8_t buf[], const int plain_len, int *cipher_len);
 void twofish_decrypt_cbc(Twofish_ctx *ctx, uint8_t buf[], const int cipher_len, int *plain_len);
+
+void twofish_init(Twofish_ctx *ctx, const uint8_t key[], int key_len);
